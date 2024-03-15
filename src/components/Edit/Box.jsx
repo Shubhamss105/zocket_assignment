8d3ebf5bf7705f5fadac0a6605ea1f84392a3b9d
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-03-15 16:00:52 +0530

    third commit

--- src/components/Edit/Box.jsx
+++ src/components/Edit/Box.jsx
@@ -11,7 +11,7 @@ function Box() {
   return (
     <div className='flex flex-col sm:w-1/2 w-screen'>
       <FontAwesomeIcon icon={faCircleXmark} className = "absolute top-20 right-20 cursor-pointer"/>
-      <h1 className='text-center text-lg sm:text-2xl font-bold mt-32'>Ad customization</h1>
+      <h1 className='text-center text-lg sm:text-2xl font-bold mt-16'>Ad customization</h1>
       <h2 className='text-center text-sm sm:text-lg mt-2 text-gray-500 text-opacity-70'>Customize your ad and get the templates accordingly</h2>
       <div className='sm:ml-20 ml-10 mt-14'><AdImg/></div> 
       {/* <h3 className='text-center text-sm sm:text-lg mt-10 text-gray-500 text-opacity-70'>Edit Contents</h3>  */}
